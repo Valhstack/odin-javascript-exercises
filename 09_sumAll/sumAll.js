3b63e4b8da18eb14a85a6f155985d7150e12f288
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-09-15 19:53:34 +0200

    Excersice 9 is finished

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let sum = 0;
+    
+    if(!Number.isInteger(firstNum) || !Number.isInteger(lastNum) || firstNum < 0 || lastNum < 0){
+        return "ERROR";
+    }
+    else{
+        if(firstNum > lastNum){
+            let temp = lastNum;
+            lastNum = firstNum;
+            firstNum = temp;
+        }
 
+        for(let i = firstNum; i <= lastNum; i++){
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
