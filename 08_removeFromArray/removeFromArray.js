894db58623c5d97d0d30b87e36fbebf3990bb1bd
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-09-15 18:59:31 +0200

    Excercise 7 is finished

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...argsToRemoveFromArr) {
+    let arrToReturn = arr;
+
+    for (let i = 0; i < arrToReturn.length; i++) {
+        for (let j = 0; j < argsToRemoveFromArr.length; j++) {
+            if (argsToRemoveFromArr[j] === arr[i]) {
+                arrToReturn.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    return arrToReturn;
 };
 
 // Do not edit below this line
