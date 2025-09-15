c8be02487c268d7355f6b33677118bda2ac9c4bb
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-09-15 18:14:28 +0200

    Resolved task #6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (incomingString, repeatNumber) {
+    let outcomingString = "";
 
+    if (repeatNumber < 0) {
+        return outcomingString = "ERROR";
+    }
+    else if (repeatNumber === 0) {
+        return outcomingString = "";
+    }
+    else {
+        for (let i = 0; i < repeatNumber; i++) {
+            outcomingString += incomingString;
+        }
+        return outcomingString;
+    }
 };
 
 // Do not edit below this line
