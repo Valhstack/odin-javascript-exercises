7e2599f3ef709f9f2ea3713a6409c738ae84a1c8
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-09-15 20:19:07 +0200

    Excersice 11 is finished

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFarenheit) {
+  let tempCelsius = (tempFarenheit - 32) * 5 / 9;
+
+  tempCelsius = Number(tempCelsius.toFixed(1));
+
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFarenheit = (tempCelsius * 9 / 5) + 32;
+
+  tempFarenheit = Number(tempFarenheit.toFixed(1));
+
+  return tempFarenheit;
 };
 
 // Do not edit below this line
