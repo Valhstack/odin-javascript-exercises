d96b25cee6f1dda4b7e612cd4532ec1150a08dbb
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-10-27 15:53:04 +0100

    Excersice 12 is finished

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let sum = 0;
+
+  for (let num of numbers) {
+    sum += num;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (args) {
+  let multiplication = 1;
 
+  for (let arg of args) {
+    multiplication *= arg;
+  }
+
+  return multiplication;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let factorial = 1;
+
+  if (num < 0) return;
+  if (num === 0) return 1;
+
+  for(let i = 1; i <= num; i++){
+    factorial *= i;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
