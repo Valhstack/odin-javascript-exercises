4db01037786f0c5ac5b4be6c9a381a522ce4b575
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-10-27 20:27:56 +0100

    Excersice 13 is finished

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    let normilised = palindrome.toLowerCase().replace(/[^a-zA-Z\d]/g, "");
 
+    let reversed = normilised.split("").reverse().join("");;
+
+    let isPalindrome = normilised === reversed ? true : false;
+
+    return isPalindrome;
 };
 
 // Do not edit below this line
