0d09b9e0657ab4e5561b22f6c5d1fa64bd3a90d3
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-10-27 22:25:01 +0100

    Excersice 16 is finished

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0], current, prev;
 
+    let date = new Date();
+    let currentYear = date.getFullYear();
+
+    if(!people[0].yearOfDeath)  prev = currentYear - people[0].yearOfBirth;
+    else prev = people[0].yearOfDeath - people[0].yearOfBirth;
+
+    for(let i = 1; i < people.length; i++){
+        current = people[i].yearOfDeath - people[i].yearOfBirth;
+
+        if(current > prev){
+            oldest = people[i];
+            prev = current;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
